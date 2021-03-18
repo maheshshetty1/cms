77634adb8f3df662cf0259214287c67a3a24aaa3
maheshshetty1 <msmaheshshetty54@gmail.com>
2021-03-18 13:20:28 +0530

    refactored database connections

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@
 if (process.env.NODE_ENV === 'prod') {
     module.exports = {
 
-        mongodbUrl: 'mongodb+srv://cms-user:cms123@cms.3uyks.mongodb.net/cms?retryWrites=true&w=majority'
+        mongodbUrl: process.env.MONGODB_URI
     }
 } else {
     module.exports = {
