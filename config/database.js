13279109e0157d9c21db64fe5612a22c5dfb94a9
maheshshetty1 <msmaheshshetty54@gmail.com>
2021-03-18 13:08:48 +0530

    refactored database connections

--- config/database.js
+++ config/database.js
@@ -1,5 +1,12 @@
-module.exports = {
 
-    //mongodbUrl : 'mongodb://localhost:27017/cms'
-    mongodbUrl : 'mongodb+srv://cms-user:cms123@cms.3uyks.mongodb.net/cms?retryWrites=true&w=majority'
+
+if (process.env.NODE_ENV === 'prod') {
+    module.exports = {
+
+        mongodbUrl: 'mongodb+srv://cms-user:cms123@cms.3uyks.mongodb.net/cms?retryWrites=true&w=majority'
+    }
+} else {
+    module.exports = {
+        mongodbUrl: 'mongodb://localhost:27017/cms'
+    }
 }
